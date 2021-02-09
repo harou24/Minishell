dc57aff63ef987df7199530f4474b10623c55206
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-02-09 16:55:02 +0100

    Continuous Integration: basics Added github workflow for catch unittests Added skeletons for Catch unittests Added basic CMakeLists.txt to root for basic-compilation checks Updated compilation-check.yml to also verify Cmake works properly Fixed bad indent in yaml files for github workflows Bumped all libraries to newest pull @ master

---
+++ tests/minishell_basics.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch.hpp>
+
+/*
+ * Catch2: examples
+ * for more examples checkout:
+ *   https://github.com/catchorg/Catch2/tree/devel/examples
+ *
+ * Catch has two natural expression assertion macro's:
+ * - REQUIRE() stops at first failure.
+ * - CHECK() continues after failure.
+ *
+ * There are two variants to support decomposing negated expressions:
+ * - REQUIRE_FALSE() stops at first failure.
+ * - CHECK_FALSE() continues after failure.
+ *
+ * TEST_CASE( "Title here", "[short name]" ) {
+ *     REQUIRE( [BOOLEAN CONDITION] ) ;
+ * }
+ */
+
+ TEST_CASE( "Title here", "[short name]" ) {
+     REQUIRE(1) ;
+ }
