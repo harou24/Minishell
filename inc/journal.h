fe83578824e143ed3dbfe6cacc9b596e035e44c2
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-02-12 04:09:13 +0100

    Added prototypes for lookup/setter functions in journal and lexer

--- inc/journal.h
+++ inc/journal.h
@@ -21,9 +21,19 @@ typedef struct			s_journal
 	t_journal_storetype	*counter;
 }						t_journal;
 
-void					*journal_add_token(t_token *token);
+void					*journal_add(t_token *token);
+void					*journal_get(size_t index);
+size_t					journal_size();
+t_bool					journal_has_token(t_token *token);
+t_bool					*journal_has_tokentype(const e_token_type type);
+t_token					*journal_find_first_of_tokentype(const e_token_type type);
+t_token					*journal_find_last_of_tokentype(const e_token_type type);
+
+/* some ..._for functions not written yet */
 
 t_journal				*journal_create(size_t size);
-t_journal				*journal_destroy(t_journal *journal);
+t_journal				*journal_create_for(size_t size);
+t_journal				*journal_destroy();
+t_journal				*journal_destroy_for(t_journal *journal);
 
 #endif
--- src/journal.c
+++ src/journal.c
@@ -2,7 +2,17 @@
 
 # define V_DEF_SIZE	(TOKEN_TYPE_INDEX + 1)
 
+static t_journal *g_journal__;
+
 t_journal        *journal_create(size_t size)
+{
+	if (g_journal__)
+		return (g_journal__);
+	else
+		return ((g_journal__ = journal_create_for(size)));
+}
+
+t_journal        *journal_create_for(size_t size)
 {
 	t_journal	*journal;
 
@@ -17,8 +27,16 @@ t_journal        *journal_create(size_t size)
 	return (journal);
 }
 
-t_journal        *journal_destroy(t_journal *journal)
+t_journal        *journal_destroy()
 {
+	journal_destroy_for(g_journal__);
+	return ((g_journal__ = NULL));
+}
+
+t_journal        *journal_destroy_for(t_journal *journal)
+{
+	if (!journal)
+		return (NULL);
 	while (*(size_t *)vector(&journal->tokens, V_SIZE, 0, 0) > 0)
 	{
 		token_destroy(vector(&journal->tokens, V_PEEKBACK, 0, 0));
@@ -29,3 +47,39 @@ t_journal        *journal_destroy(t_journal *journal)
 	free(journal);
 	return (NULL);
 }
+
+void					*journal_add(t_token *token)
+{
+
+}
+
+void					*journal_get(size_t index)
+{
+
+}
+
+size_t					journal_size()
+{
+
+}
+
+t_bool					journal_has_token(t_token *token)
+{
+
+}
+
+t_bool					*journal_has_tokentype(const e_token_type type)
+{
+
+}
+
+t_token					*journal_find_first_of_tokentype(const e_token_type type)
+{
+
+}
+
+t_token					*journal_find_last_of_tokentype(const e_token_type type)
+{
+
+}
+
