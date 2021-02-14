0bad6ccd7c80ec7bf1db67400d6984a0f44d82af
Harou <haroutioun24@gmail.com>
2021-02-14 20:37:22 +0100

    command.h

---
+++ includes/command.h
@@ -0,0 +1,17 @@
+#ifndef COMMAND_H
+# define COMMAND_H
+
+typedef struct	s_command
+{
+		int	code_error;
+		char	*input;
+		char	*output;
+}		t_command;
+
+t_command	*command_create(const char *_input);
+int		command_execute(t_command *_command);
+int		command_get_code_error(t_command *_command);
+void		command_print_output(t_command *_command);
+void		command_destroy(t_command *_command);
+
+#endif
