ea49f59d6ff3c33694bca4bbbcf89f73ab21c957
Harou <haroutioun24@gmail.com>
2021-02-15 12:54:25 +0100

    update : BUFFER_SIZE -> __DIR_BUFF_SIZE

--- includes/directory.h
+++ includes/directory.h
@@ -4,8 +4,6 @@
 #include "libft.h"
 #include <unistd.h>
 
-#define BUFFER_SIZE 255
-
 const char	*directory_get_current_dir(void);
 
 #endif
--- src/directory.c
+++ src/directory.c
@@ -1,8 +1,10 @@
 #include "directory.h"
 
+#define __DIR_BUFF_SIZE 255
+
 const char	*directory_get_current_dir(void)
 {
-	char	buffer[BUFFER_SIZE];
+	char	buffer[__DIR_BUFF_SIZE];
 
 	return (getcwd(buffer, sizeof(buffer)));
 }
