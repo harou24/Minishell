10ac7bf8843896ac379c429a56fa566d04534699
Haroutioun <61208023+harou24@users.noreply.github.com>
2021-02-15 15:09:23 +0100

    get_current_directory() (#48)
    * get_current_directory()
    
    * update changes, allocating on the heap, change return value of get_current_dir()
    
    * back to stack allocation for the buffer
    
    * update : BUFFER_SIZE -> __DIR_BUFF_SIZE
    
    * update on directory.h directory.c and directory.cpp

---
+++ tests/src/directory.cpp
@@ -0,0 +1,33 @@
+#include <catch2/catch.hpp>
+
+/*
+ * Catch2: examples
+ * for more examples checkout:
+ *   https://github.com/catchorg/Catch2/tree/devel/examples
+ *
+ * Catch has two natural expression assertion macro's:
+ * - REQUIRE() stops at first failure.
+ * - CHECK() continues after failure.
+ *
+ * There are two variants to support decomposing negated expressions:
+ * - REQUIRE_FALSE() stops at first failure.
+ * - CHECK_FALSE() continues after failure.
+ *
+ * TEST_CASE( "Title here", "[short name]" ) {
+ *     REQUIRE( [BOOLEAN CONDITION] ) ;
+ * }
+ */
+extern "C" {
+# include "directory.h"
+}
+
+#include <string.h>
+#include <unistd.h>
+
+ TEST_CASE( "directory", "[dir]" ) {
+	 char *cur_dir = directory_get_current_dir();
+	 char buffer[1024];
+	 getcwd(buffer, 1024);
+	 REQUIRE(strcmp(cur_dir, buffer) == 0);
+	 free(cur_dir);
+ }
