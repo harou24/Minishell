d3ca036f4faef5670e73739dd7a08198e55ca28b
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-02-16 14:17:46 +0100

    Polished up minimake.sh: added 'clean' target

--- minimake.sh
+++ minimake.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
+build_dir=build
+
 if [ $# -eq 1 ] && [ "$1" == "test" ]; then
 	# compile and test
-	cmake -S . -B build -DBUILD_TESTING=ON && ( cd build && make ) \
-	&& ctest --output-on-failure ./build
+	cmake -S . -B $build_dir -DBUILD_TESTING=ON && ( cd $build_dir && make ) \
+	&& ( cd $build_dir && ctest --output-on-failure ./)
+elif [ $# -eq 1 ] && [ "$1" == "clean" ]; then
+	# clean all files which are specified in .gitignore 
+	git clean -d -f -X
 elif [ $# -eq 0 ]; then
-	# compile and copy minishell binary to root folder
-	cmake -S . -B build -DBUILD_TESTING=OFF && ( cd build && make ) \
-	&& cp build/apps/minishell ./ \
-	&& echo -e "You can run ./\e[92mminishell\e[39m now."
+	# compile and copy minishell binary and lib to root folder
+	cmake -S . -B $build_dir -DBUILD_TESTING=OFF && ( cd $build_dir && make ) \
+	&& cp $build_dir/apps/minishell ./ \
+	&& cp $build_dir/src/libminishell.a ./ \
+	&& echo -e "You can run ./\e[92mminishell\e[39m now or include \e[92mlibminishell.a\e[39m in your library."
 else
-	echo "usage: '$0 test' for testing or '$0' to only compile minishell"
+	cat<<-EOF
+	usage:
+	  $0       -> compile
+	  $0 test  -> compile and test
+	  $0 clean -> remove build files 
+	EOF
 fi
 
