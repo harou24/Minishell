155f016c313657657f81f24c20014a14512916a0
Harou <haroutioun24@gmail.com>
2021-02-16 11:48:07 +0100

    work in progress, command_execute(),command_get_input(), command_destroy() update

--- includes/command.h
+++ includes/command.h
@@ -1,18 +1,20 @@
 #ifndef COMMAND_H
 # define COMMAND_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include "libft.h"
-
 typedef struct	s_command
 {
 		int	code_error;
 		char	*input;
 		char	*output;
+		char	*binary_path;
+		char	**argv;
+		int	argc;
+		char	*env;
 }		t_command;
 
 t_command	*command_create(const char *_input);
+char		*command_get_input(t_command *_command);
+char		*command_get_output(t_command *_command);
 int		command_execute(t_command *_command);
 int		command_get_code_error(t_command *_command);
 void		command_print_output(t_command *_command);
--- src/command.c
+++ src/command.c
@@ -1,4 +1,8 @@
 #include "command.h"
+#include "libft.h"
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 t_command	*command_create(const char *_input)
 {
@@ -13,12 +17,49 @@ t_command	*command_create(const char *_input)
 	return(command);
 }
 
-int		command_execute(t_command *_command)
+char		*command_get_input()
 {
-	/* execute the command and store the output in command->output*/
-	/* return 0 if success, otherwise return CODE_ERROR*/
-	(void)_command;
-	return (0);
+	char	*input;
+	char	*line;
+	int 	size;
+
+	input = NULL;
+	line = NULL;
+	size = 0;
+	while (get_next_line(stdin, &line) > 0)
+	{
+		size = ft_strlen(input) + ft_strlen(line);
+		if (ft_realloc(input, 1, size))
+			input = ft_strlcat(input, line, size);
+	}
+	return (input);
+}
+
+void		command_execute(t_command *_command)
+{
+	pid_t	pid;
+	int	status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		_command->error_code = errno;
+		printf(strerror(errno));
+	}
+	else if (pid > 0)
+	{
+		waitpid(pid, &status, 0);
+		kill(pid, SIGTERM);
+	}
+	else
+	{
+		if (execve(_command->binary_path, _command->argv, _command->env) == -1)
+		{
+			_command->error_code = errno;
+			printf(strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
 int		command_get_code_error(t_command *_command)
@@ -37,6 +78,11 @@ void		command_destroy(t_command *_command)
 		free(_command->input);
 	if (_command->output)
 		free(_command->output);
-	if (_command)
-		free(_command);
+	if (_command->binary_path)
+		free(_command->binary_path);
+	if (_command->argv)
+		ft_destroy_array(_command->argv, sizeof(char *), _command->argc);
+	if (_command->env)
+		free(_command->env);
+	free(_command);
 }
