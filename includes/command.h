0a5f3f4398074bd363f07010712a9306306b200a
Harou <haroutioun24@gmail.com>
2021-02-18 14:32:42 +0100

    clean up what was not needed and update->command_run_builtins()

--- includes/command.h
+++ includes/command.h
@@ -7,16 +7,22 @@ typedef struct	s_command
 		int	code_error;
 		char	**argv;
 		int	argc;
-		char	**env;
-		int	is_built_int;
 }		t_command;
 
-t_command	*command_create();
-int		command_read_input(t_command *_command);
-char		*command_get_input(t_command *_command);
-char		**get_environment();
+t_command	*command_create(const char *_input);
 void		command_execute(t_command *_command);
 int		command_get_code_error(t_command *_command);
 void		command_destroy(t_command *_command);
+
+/*builtins*/
+
+int		command_run_builtins(t_command *_command);
+int		command_run_echo(t_command *_command);
+int		command_run_cd(t_command *_command);
+int		command_run_pwd(t_command *_command);
+int		command_run_unset(t_command *_command);
+int		command_run_export(t_command *_command);
+int		command_run_env(t_command *_command);
+int		command_run_exit(t_command *_command);
 
 #endif
--- src/command.c
+++ src/command.c
@@ -7,36 +7,25 @@
 #include <string.h>
 #include <sys/wait.h>
 
-t_command	*command_create()
+t_command	*command_create(const char *_input)
 {
 	t_command *command;
 
+	command->input = ft_strdup(_input);
+	if (!command->input)
+		return (NULL);
 	command = ft_calloc(sizeof(t_command), 1);
 	if (!command)
-		return (NULL);
-	if (command_read_input(command) < 1)
 		return (NULL);
 	command->argv = ft_split(command->input, ' ');
 	if (!command->argv)
 		return (NULL);
 	command->code_error = 0;
-	command->env = NULL;
-	command->argc = 1;
+	command->argc = ft_str_array_length();
 	return(command);
 }
 
-int		command_read_input(t_command *_command)
-{
-	return (get_next_line(STDIN, &_command->input));
-}
-
-char 		*command_get_input(t_command *_command)
-{
-	return (_command->input);
-}
-
-
-void		command_execute(t_command *_command)
+void		command_execute(t_command *_command, char **_env)
 {
 	pid_t	pid;
 	int	status;
@@ -54,7 +43,7 @@ void		command_execute(t_command *_command)
 	}
 	else
 	{
-		if (execve(_command->argv[0], _command->argv, _command->env) == -1)
+		if (execve(_command->argv[0], _command->argv, _env) == -1)
 		{
 			_command->code_error = errno;
 			printf(strerror(errno));
@@ -68,7 +57,6 @@ int		command_get_code_error(t_command *_command)
 	return (_command->code_error);
 }
 
-
 void		command_destroy(t_command *_command)
 {
 	if (_command->input)
@@ -76,13 +64,25 @@ void		command_destroy(t_command *_command)
 	if (_command->output)
 		free(_command->output);
 	if (_command->argv)
-	{
-	/*	ft_destroy_array((void **)_command->argv, sizeof(void*), _command->argc);*/
-	free(_command->argv[0]);
-	free(_command->argv);
-
-	}
+		ft_destroy_array((void **)_command->argv, sizeof(void*), _command->argc);
 	if (_command->env)
 		free(_command->env);
 	free(_command);
+}
+
+int		command_run_builtins(t_command *_command)
+{
+	if (ft_strcmp(_command->argv[0], "echo") == 0)
+		return (command_run_echo(_command));
+	if (ft_strcmp(_command->argv[0], "cd") == 0)
+		return (command_run_cd(_command));
+	if (ft_strcmp(_command->argv[0], "pwd") == 0)
+		return (command_run_pwd(_command));
+	if (ft_strcmp(_command->argv[0], "export") == 0)
+		return (command_run_export(_command));
+	if (ft_strcmp(_command->argv[0], "env") == 0)
+		return (command_run_env(_command));
+	if (ft_strcmp(_command->argv[0], "exit") == 0)
+		return (command_run_exit(_command));
+	return (0);
 }
