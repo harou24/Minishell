d18e1576fdbd45176bf5b0aca32cc80d17b80258
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-02-25 14:38:18 +0100

    bash_pattern.c/h: added basic pattern matching for fixed + stub

--- includes/bash_pattern.h
+++ includes/bash_pattern.h
@@ -1,6 +1,7 @@
 #ifndef BASH_PATTERN_H
 # define BASH_PATTERN_H
 
+#include "range.h"
 #include "journal.h"
 #include "token.h"
 
@@ -21,10 +22,12 @@ typedef enum					s_bash_pattern_type
 typedef struct					s_bash_pattern
 {
 	const t_bash_pattern_type	pattern_type;
+	const size_t				fixed_types_len;
 	const e_token_type			fixed_types[B_PAT_MAXLEN];
+	const size_t				fuzzy_types_len;
 	const e_token_type			fuzzy_types[B_PAT_MAXLEN];
 }								t_bash_pattern;
 
-t_bash_pattern_type				batch_match_pattern(t_journal *journal, size_t start, size_t len);
+t_bash_pattern_type				batch_match_pattern(t_journal *journal, t_range range);
 
 #endif
--- src/bash_pattern.c
+++ src/bash_pattern.c
@@ -1,17 +1,68 @@
 #include <stdlib.h>
+#include <assert.h>
 #include "bash_pattern.h"
 
-static const t_bash_pattern bash_pat_list[] =	{
-													{ P_COMMAND, {WORD, SPACE}, {WORD, SPACE} },
-													{ P_ASSIGNMENT, {WORD, SPACE, ASSIGNMENT, WORD}, {} }
-												};
+static const t_bash_pattern pat_list[] =	{
+												{ P_COMMAND, 2, {WORD, SPACE}, 2, {WORD, SPACE} },
+												{ P_ASSIGNMENT, 4, {WORD, SPACE, ASSIGNMENT, WORD}, 0, {} }
+											};
 
-t_bash_pattern_type		batch_match_pattern(t_journal *journal, size_t start, size_t len)
+t_bool					match_fuzzy(t_range range, const e_token_type *fixed_types, size_t fixed_types_len)
 {
 	/* stub */
+	(void)range;
+	(void)fixed_types;
+	(void)fixed_types_len;
+	return (FALSE);
+}
+
+t_bool					match_fixed(t_range range, const e_token_type *fixed_types, size_t fixed_types_len)
+{
+	size_t				i;
+	t_token				*token;
+
+	if (range.end >= journal_size() || range.end + range.begin > fixed_types_len)
+		return (FALSE);
+	range.end += fixed_types_len;
+	i = 0;
+	while (range.begin + i <= range.end)
+	{
+		token = journal_get(range.begin + i);
+		assert(token);
+		if (fixed_types[i] != token->type)
+			return (FALSE);
+		i++;	
+	}
+	return (TRUE);
+}
+
+t_bool					is_bash_pattern(t_range range, const t_bash_pattern *pattern)
+{
+	return (match_fixed(range, pattern->fixed_types, pattern->fixed_types_len)
+		&& match_fuzzy(range, pattern->fuzzy_types, pattern->fuzzy_types_len));
+}
+
+t_bash_pattern_type		batch_match_pattern(t_journal *journal, t_range range)
+{
+	const size_t	pat_list_size = sizeof(pat_list)/sizeof(pat_list[0]);
+	t_bash_pattern	*candidate;
+	size_t			i;
+
+	candidate = NULL;
+	i = 0;
+	while (i < pat_list_size)
+	{
+		if (is_bash_pattern(range, &pat_list[i]))
+		{
+			/* we found a match */
+			if (candidate == NULL || pat_list[i].fixed_types_len > candidate->fixed_types_len)
+			{
+				/* this candidate is better */
+				candidate = (t_bash_pattern *)&pat_list[i];
+			}
+		}
+		i++;
+	}
+	return ((candidate == NULL) ? P_NO_TYPE : candidate->pattern_type);
 	(void)journal;
-	(void)start;
-	(void)len;
-	(void)bash_pat_list;
-	return (P_NO_TYPE);
 }
