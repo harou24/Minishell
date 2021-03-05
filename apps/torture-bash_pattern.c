097c8eeda2a1f8107500b698ee8a43cefd206d2e
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-05 16:26:24 +0100

    apps/torture-bash_pattern.c: only tests bash_pattern (no further parsing)

--- apps/torture-bash_pattern.c
+++ apps/torture-bash_pattern.c
@@ -21,25 +21,32 @@ int main (int argc, char **argv)
 			printf("%30s : %s\n", "LEX TOKENS", for_human);
 			free(for_human);
 
-			//parse_expand_strings(journal_create());
-			parse(journal_create());
+			if (!parse_expand())
+			{
+				printf("PARSE EXPAND FAILED\n");
+				err = true;
+			}
+			else
+			{
+				journal_remove_tokens_with_type(SPACE);
 
-			for_human = journal_dump_tokens();
-			printf("%30s : %s\n", "POST-PARSE TOKENS", for_human);
-			free(for_human);
+				for_human = journal_dump_tokens();
+				printf("%30s : %s\n", "POST-PARSE TOKENS", for_human);
+				free(for_human);
 
-			for_human = journal_reconstruct_string();
-			printf("%30s : %s\n", "RECONSTRUCTED STRING", for_human);
-			free(for_human);
+				for_human = journal_reconstruct_string();
+				printf("%30s : %s\n", "RECONSTRUCTED STRING", for_human);
+				free(for_human);
 
-			char *endptr;
-			int match_len = strtod(argv[2], &endptr);
-			assert(*endptr == '\0');
-			assert(match_len > 0);
+				char *endptr;
+				int match_len = strtod(argv[2], &endptr);
+				assert(*endptr == '\0');
+				assert(match_len > 0);
 
-			t_bash_pattern_type pattern = bash_match_pattern(range(0, match_len - 1));
-			for_human = pattern_dump_type(pattern);
-			printf("%30s : %s\n", "MATCHED COMMAND", for_human);
+				t_bash_pattern_type pattern = bash_match_pattern(range(0, match_len - 1));
+				for_human = pattern_dump_type(pattern);
+				printf("%30s : %s\n", "MATCHED COMMAND", for_human);
+			}
 		}
 		else
 		{
