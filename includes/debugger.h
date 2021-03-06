7baf5fe958bfb987241c1201280a9a4a36832e6f
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-06 12:39:17 +0100

    removed comment

--- includes/debugger.h
+++ includes/debugger.h
@@ -16,10 +16,7 @@ t_bool				debug_init_tofd(int fd);
 void				debug_deinit();
 int					debug(const char *format, ...);
 
-/* #define dbg(fmt, ...) do { if (DEBUG) { debug(__VA_ARGS__); } } while (0) */
-
 /* https://stackoverflow.com/questions/1644868/define-macro-for-debug-printing-in-c */
-
 #define dbg(fmt, ...) \
         do { if (DEBUG) debug("%s:%d:%s(): " fmt, __FILE__, \
                                 __LINE__, __func__, __VA_ARGS__); } while (0)
