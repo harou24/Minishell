0727c6e01bdd15d07f09eee7ccbcfd4a8bcb69f1
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-08 16:50:15 +0100

    executor.c/h: setup basic stubs, optabs

---
+++ src/executor.c
@@ -0,0 +1,52 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "libft.h"
+#include "debugger.h"
+
+#include "command.h"
+#include "executor.h"
+
+typedef struct		s_op
+{
+	t_exec_op_type	type;
+	int				(*f)(t_command *cmd);
+}					t_op;
+
+static int			op_stub(t_command *cmd) {(void)cmd; return (-1);}
+
+static int	(*g_optab__[OP_TAB_SIZE])(t_command *cmd) =	{
+	[OP_COMMAND] = op_stub,
+	[OP_PATH] = op_stub,
+	[OP_ASSIGNMENT] = op_stub,
+	[OP_BUILTIN_ECHO] = op_stub,
+	[OP_BUILTIN_CD] = op_stub,
+	[OP_BUILTIN_PWD] = op_stub,
+	[OP_BUILTIN_EXPORT] = op_stub,
+	[OP_BUILTIN_UNSET] = op_stub,
+	[OP_BUILTIN_ENV] = op_stub,
+	[OP_BUILTIN_EXIT] = op_stub,
+	[OP_NO_TYPE] = op_stub
+									};
+
+int		execute(t_execscheme *scheme)
+{
+	int	error;
+
+	error = 0;
+	while (scheme)
+	{
+		/*
+			1. handle op type
+			2. setup fd's
+			3. fork()
+			4. execute
+		*/
+		g_optab__[scheme->op_type](scheme->cmd);
+		scheme = scheme->next;
+	}
+	return (error);
+}
