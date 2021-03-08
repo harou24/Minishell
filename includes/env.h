7f1e383078d22ef7effe6ea6ea2f87adb6c9ab41
Harou <haroutioun24@gmail.com>
2021-03-08 20:09:21 +0100

    env.h/env.c removed '_' before param names

--- includes/env.h
+++ includes/env.h
@@ -4,19 +4,20 @@
 #include "pair.h"
 #include "hashmap.h"
 
-typedef struct	s_env {
-		void	*hm_store;
-		size_t	store_size;
+typedef struct	senv
+{
+	void	*hm_store;
+	size_t	store_size;
 }		t_env;
 
-t_env	*env_create(const char **_env);
-char	*env_get(t_env *env, const char *_var);
-void	*env_set(t_env *env, char *_var, char *_value);
-void	env_unset(t_env *env, char *_var);
+t_env	*env_create(const char **env);
+char	*env_get(t_env *env, const char *key);
+void	*env_set(t_env *env, char *key, char *value);
+void	env_unset(t_env *env, char *key);
 size_t	env_size(t_env *env);
 void	env_destroy(t_env *env);
-void	*set_env_array(t_env *_this_env, const char **_env);
-int	put_env_line_into_store(t_env *_env, const char *_env_line, void *_hm_store);
-t_pair	*split_line_into_key_value_pair(const char *_line);
+void	*set_env_array(t_env *this_env, const char **env);
+int	put_env_line_into_store(t_env *env, const char *env_line, void *hm_store);
+t_pair	*split_line_into_key_value_pair(const char *line);
 
 #endif
--- src/env.c
+++ src/env.c
@@ -4,16 +4,16 @@
 
 #define HASHMAP_SIZE 1000
 
-t_pair	*split_line_into_key_value_pair(const char *_line)
+t_pair	*split_line_into_key_value_pair(const char *line)
 {
 	t_pair	*pair;
 	int	equal_sign_index;
 	char	*key;
 	char	*value;
 
-	equal_sign_index = ft_strclen(_line, '=');
-	key = ft_strsub(_line, 0, equal_sign_index);
-	value = ft_strsub(_line, equal_sign_index + 1, ft_strlen(_line) - equal_sign_index);
+	equal_sign_index = ft_strclen(line, '=');
+	key = ft_strsub(line, 0, equal_sign_index);
+	value = ft_strsub(line, equal_sign_index + 1, ft_strlen(line) - equal_sign_index);
 	pair = pair_create(key, value);
 	if (!pair)
 	{
@@ -23,15 +23,15 @@ t_pair	*split_line_into_key_value_pair(const char *_line)
 	return (pair);
 }
 
-int	put_env_line_into_store(t_env *_env, const char *_env_line, void *_hm_store)
+int	put_env_line_into_store(t_env *env, const char *envline, void *hm_store)
 {
 	t_pair	*pair;
 
-	pair = split_line_into_key_value_pair(_env_line);
-	if (!pair || !hm_set(_hm_store, pair->f.key, pair->s.value))
+	pair = split_line_into_key_value_pair(envline);
+	if (!pair || !hm_set(hm_store, pair->f.key, pair->s.value))
 	{
 		free(pair->f.key);
-		env_destroy(_env);
+		env_destroy(env);
 		return (0);
 	}
 	free(pair->f.key);
@@ -39,7 +39,7 @@ int	put_env_line_into_store(t_env *_env, const char *_env_line, void *_hm_store)
 	return (1);
 }
 
-void	*set_env_array(t_env *_this_env, const char **_env)
+void	*set_env_array(t_env *this_env, const char **env)
 {
 	void		*hm_store;
 	int		count;
@@ -48,9 +48,9 @@ void	*set_env_array(t_env *_this_env, const char **_env)
 	if (!hm_store)
 		return (NULL);
 	count = 0;
-	while(_env[count])
+	while(env[count])
 	{
-		if (!put_env_line_into_store(_this_env, _env[count], hm_store))
+		if (!put_env_line_into_store(this_env, env[count], hm_store))
 			return (NULL);
 		count++;
 	}
@@ -58,43 +58,43 @@ void	*set_env_array(t_env *_this_env, const char **_env)
 }
 
 
-t_env	*env_create(const char **_env)
+t_env	*env_create(const char **env)
 {
-	t_env	*env;
+	t_env	*this_env;
 
-	env = ft_calloc(sizeof(t_env), 1);
-	if (!env)
+	this_env = ft_calloc(sizeof(t_env), 1);
+	if (!this_env)
 		return (NULL);
-	env->store_size = ft_arraylen((const void**)_env);
-	env->hm_store = set_env_array(env, _env);
-	if (!env->hm_store)
+	this_env->store_size = ft_arraylen((const void**)env);
+	this_env->hm_store = set_env_array(this_env, env);
+	if (!this_env->hm_store)
 		return (NULL);
-	return (env);
+	return (this_env);
 }
 
-char	*env_get(t_env *env, const char *_var)
+char	*env_get(t_env *env, const char *key)
 {
-	return ((char *)hm_get(env->hm_store, _var));
+	return ((char *)hm_get(env->hm_store, key));
 }
 
-void	*env_set(t_env *_env, char *_var, char *_value)
+void	*env_set(t_env *env, char *key, char *value)
 {
-	return (hm_set(_env->hm_store, _var, _value));
+	return (hm_set(env->hm_store, key, value));
 }
 
-void	env_unset(t_env *_env, char *_var)
+void	env_unset(t_env *env, char *key)
 {
-	hm_remove(_env->hm_store, _var, free);
+	hm_remove(env->hm_store, key, free);
 }
 
-size_t	env_size(t_env *_env)
+size_t	env_size(t_env *env)
 {
-	return (_env->store_size);
+	return (env->store_size);
 }
 
-void	env_destroy(t_env *_env)
+void	env_destroy(t_env *env)
 {
-	if (_env->hm_store)
-		hm_destroy(_env->hm_store, free);
-	free(_env);
+	if (env->hm_store)
+		hm_destroy(env->hm_store, free);
+	free(env);
 }
