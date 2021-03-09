1d52b69bfcc50e7aa79bf989f2e41e49150d36e5
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-09 17:12:55 +0100

    file.c/h: added file_exists() and file_is_executable(), fixes #124

---
+++ includes/file.h
@@ -0,0 +1,9 @@
+#ifndef FILE_H
+# define FILE_H
+
+#include "libft.h"
+
+t_bool	file_exists(const char *filename);
+t_bool	file_is_executable(const char *filename);
+
+#endif
---
+++ src/file.c
@@ -0,0 +1,19 @@
+#include <sys/stat.h>
+
+#include "file.h"
+
+t_bool	file_exists (const char *filename)
+{
+ 	struct stat	buffer;
+	
+ 	return (stat(filename, &buffer) == 0 && S_ISREG(buffer.st_mode));
+}
+
+t_bool	file_is_executable(const char *filename)
+{
+	struct stat	buffer;
+	
+ 	return (stat(filename, &buffer) == 0
+			&& S_ISREG(buffer.st_mode)
+			&& buffer.st_mode & S_IXUSR);
+}
