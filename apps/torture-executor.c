7d79bb5b60a5c12784dcbddcbace21e8da6ff604
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2021-03-09 19:24:48 +0100

    Executor (#108)
    * command.c/h: moved input/output fds into single var
    
    * executor.c/h: setup basic stubs, optabs
    
    * Added torture-executor.c test for execution testing
    
    * src/parser.c: PROBLEM -> parse_build_execscheme cannot differentiate between path & command
    
    * pipe.c/h: utility to setup i/o filedescriptors for pipe
    
    * shell.c/h: skeleton by Harou
    
    * minishell.c/h, shellcore.c/h: added highest abstraction runtime
    
    * apps/minishell.c: added main.c for calling ./minishell
    
    * apps/CMakeLists.txt: added 'libpair' dependency
    
    * env_access.c/h: applied singleton logic to access functions
    
    * env_singleton.c/h: env_deinit() takes t_env **, cleanup
    
    * prompt_singleton.c/h: prompt_deinit takes t_prompt **, env access, cleanup
    
    * src/prompt.c: added assertions in prompt_create() for username and hostname
    
    * src/shellcore.c: added env_singleton logic, expanded _shell_loop() to minimal viable state
    
    * src/prompt.c: fixed bug in printing of error code
    
    * src/executor.c: added pre-merge comments for later
    
    Co-authored-by: Harou <haroutioun24@gmail.com>

---
+++ apps/torture-executor.c
@@ -0,0 +1,58 @@
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "lexer.h"
+#include "parser.h"
+#include "executor.h"
+
+int main (int argc, char **argv)
+{
+	if (argc >=  2)
+	{
+		bool err = FALSE;
+		t_lex *lexer = lex_create();
+		t_parser *parser = parser_create();
+		for (int i = 1; i < argc; i++) {
+			if (lex(argv[i]))
+			{
+				char *for_human = journal_dump_tokens();
+				printf("%30s : %s\n", "LEX TOKENS", for_human);
+				free(for_human);
+
+				t_execscheme *rootscheme;
+				rootscheme = parse(journal_create());
+				if (!rootscheme) {
+					printf("%30s\n", "PARSE FAILED");
+					break;
+				}
+				execscheme_pretty_dump(rootscheme, 15);
+
+				for_human = journal_dump_tokens();
+				printf("%30s : %s\n", "POST-PARSE TOKENS", for_human);
+				free(for_human);
+
+				for_human = journal_reconstruct_string();
+				printf("%30s : %s\n", "RECONSTRUCTED STRING", for_human);
+				free(for_human);
+
+				int error = execute(rootscheme);
+				printf("%30s: %i\n", "COMMAND", error);
+			}
+			else
+			{
+				printf("ERROR\n");
+				err = TRUE;
+				break;
+			}
+		}
+		lex_destroy(&lexer);
+		parser_destroy(&parser);
+		return (err);
+	}
+	else
+	{
+		return (1);
+	}
+}
---
+++ includes/pipe.h
@@ -0,0 +1,6 @@
+#ifndef PIPE_H
+#define PIPE_H
+
+int		ft_pipe(int pipefd[2]);
+
+#endif
