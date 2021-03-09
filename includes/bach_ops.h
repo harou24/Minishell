33dd18f298cb28a5eccb42b7219d8a56243e5655
Harou <haroutioun24@gmail.com>
2021-03-09 11:51:33 +0100

    fixed the misspeled errors and remove t_env * -> use singleton

--- includes/bach_ops.h
+++ includes/bach_ops.h
@@ -1,10 +1,12 @@
-#ifndef BACH_OPS
-# define BACH_OPS
+#ifndef BASH_OPS
+# define BASH_OPS
 
-int	builtin_echo(t_command *cmd, t_env *env);
+#include "command.h"
+
+int	builtin_echo(t_command *cmd);
 int	builtin_exit(t_command *cmd);
-int 	builtin_set_env(t_command *cmd, t_env *env);
-int	builtin_unset_env(t_command *cmd, t_env *env);
+int 	builtin_set(t_command *cmd);
+int	builtin_unset(t_command *cmd);
 int	builtin_export(t_command *cmd);
 int	builtin_cd(t_command *cmd);
 int	builtin_pwd(t_command *cmd);
