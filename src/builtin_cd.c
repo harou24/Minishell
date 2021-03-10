e4009320d4b0f5b053335fcc1f7791cdadd14259
Harou <haroutioun24@gmail.com>
2021-03-10 10:44:27 +0100

    builtin_cd.c

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -1,7 +1,17 @@
 #include "bash_ops.h"
+#include "env_access"
+
 
 int	builtin_cd(t_command *cmd)
 {
 	if (cmd->argv->argc == 1)
-		return(directory_change_dir());
+		return(directory_change_dir(env_get_home()));
+	else
+	{
+		if (ft_strcmp(cmd->argv->argv[1], ".") == 0)
+			return(directory_change_dir(env_get_current_dir()));
+		if (ft_strcmp(cmd->argv->argv[1], "..") == 0)
+			return(directory_change_dir(env_get_current_dir()));
+	}
+	return(directory_change_dir(cmd->argv->argv[1]));
 }
