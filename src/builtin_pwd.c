3f5d61115eeac58cc955d5640c0a5b742451746e
Harou <haroutioun24@gmail.com>
2021-03-11 18:19:29 +0100

    pwd fixed

--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -1,12 +1,13 @@
 #include "bash_ops.h"
 #include "env_access.h"
 #include "ft_printf.h"
+#include "filesystem_traversal.h"
 
 int	builtin_pwd(t_command *cmd)
 {
 	char	*pwd;
 
-	pwd = env_get_cur_dir();
+	pwd = fs_get_cur_dir_name();
 	if (!pwd)
 		return (-1);
 	ft_dprintf(cmd->fds[FD_OUT], "%s\n", pwd);
