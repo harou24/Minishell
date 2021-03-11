4b278dc9208bdb624804a7468366cdacc2dcc1a4
Harou <haroutioun24@gmail.com>
2021-03-11 12:12:03 +0100

    clean up, removed get_parent_dir

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -4,22 +4,6 @@
 #include "ft_printf.h"
 #include <stdlib.h>
 
-/* maybe put this in env_access */
-char	*get_parent_dir()
-{
-	char *current = env_get_current_dir();
-	char *parent_dir = ft_substr(current, 0, ft_strrchr(current, '/'));
-	return (parent_dir);
-}
-
-int	go_to_parent_dir()
-{
-	char *parent_dir = get_parent_dir();
-	int ret = directory_change_dir(parent_dir);
-	free(parent_dir);
-	return (ret);
-}
-
 int 	go_to_prev_dir()
 {
 	return (directory_change_dir(env_get_prev_dir()));
@@ -35,35 +19,14 @@ int	go_to_path(const char *path)
 	return (directory_change_dir(path));
 }
 
-int	go_to_dots(const char *dots)
-{
-
-	int	nb_dots;
-	int	count;
-	int	ret;
-
-	nb_dots = ft_strlen(dots);
-	count = 1;
-	if (nb_dots == count)
-		return (go_to_path(env_get_current_dir()));
-	while (count < nb_dots)
-	{
-		ret = go_to_parent_dir();
-		if (ret != 0)
-			return (ret);
-		count++;
-	}
-	return (ret);
-}
-
 int	builtin_cd(t_command *cmd)
 {
 	if (cmd->argv->argc == 1)
 		return(go_to_home());
 	else
 	{
-		if (dots_only(cmd->argv->argv[1]))
-			return (go_to_dots(cmd->argv->argv[1]));
+		if (cmd->argv->argv[1][0] == '.')
+			return (go_to_path(cmd->argv->argv[1]));	/*check_for_real_path*/
 		else if (ft_strcmp(cmd->argv->argv[1], "-"))
 		{
 			int ret = go_to_prev_dir();
