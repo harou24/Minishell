487b30dd9a63f51e9bbaffa775d7f2d698d0960c
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-11 18:40:12 +0100

    executor.c: expanded a bit

--- src/executor.c
+++ src/executor.c
@@ -17,6 +17,7 @@ int		execute(t_execscheme *scheme)
 {
 	int		error;
 	pid_t	pid;
+	pid_t	pid_prev;
 
 	error = 0;
 	while (scheme)
@@ -27,29 +28,37 @@ int		execute(t_execscheme *scheme)
 			3. fork()
 			4. execute
 		*/
+
+
+		/* prepare execscheme */
+		if (!exec_prepare_execscheme_dispatch(scheme->relation_type)(scheme))
+		{
+			/* failed to prepare execscheme */
+		}
+
+		pid_prev = pid_last();
 		pid = fork();
 		if (pid == 0)
 		{
 			/* child */
-			/*g_optab__[scheme->op_type](scheme->cmd); */
+			if (scheme->prev && scheme->prev->relation_type == REL_SEQ)
+				pid_wait(pid_prev);
+			
+			/* call command */
+			exec_command_dispatch(scheme->op_type)(scheme->cmd);
 			dbg("%s\n", "child process didn't exit!");
 			abort();
 		}
 		else
 		{
 			/* parent */
+
 			/* store pid in vec */
 			pid_push(pid_allocate(pid));
 		}
-
-		/* handle error */
-	
-		/* if REL_SEQ -> wait for pid to finish, this is very slow -> wait in child process! */
-		if (scheme->relation_type == REL_SEQ)
-			pid_wait(pid);
-
 		scheme = scheme->next;
 	}
+
 	/* wait for all child processes to finish,  or kill processes when process returns non-zero */
 	error = pid_wait_all();
 	pid_kill_all();
