4079085a206e1b4dfb6716bc7a1e72d32e6579f1
Harou <haroutioun24@gmail.com>
2021-03-12 12:58:50 +0100

    unset-> fixed bugs

--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -10,24 +10,15 @@ int	builtin_unset(t_command *cmd)
 	if (cmd->argv->argc < 2)
 	{
 		/*later use error handler ?*/
-		ft_dprintf(cmd->fds[FD_OUT], "unset: not enough arguments");
+		ft_dprintf(STDERR, "unset: not enough arguments");
 		return (-1);
 	}
-	count = 0;
+	count = 1;
 	exit_status = EXIT_SUCCESS;
 	while (count < cmd->argv->argc)
 	{
 		if (!env_unset_s(cmd->argv->argv[count]))
-		{
-			/*
-			man unset says :  	EXIT STATUS
-
-						0    All name operands were successfully unset.
-
-       						>0    At least one name could not be unset.
-			*/
 			exit_status = EXIT_FAILURE;
-		}
 		count++;
 	}
 	exit(exit_status);
