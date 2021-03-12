2971d88950f6c20b599890f9dacfe1f423f82af2
Harou <haroutioun24@gmail.com>
2021-03-12 10:39:54 +0100

    builtin_cd updated

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -1,41 +1,49 @@
 #include "bash_ops.h"
 #include "env_access.h"
-#include "directory.h"
+#include "filesystem_traversal.h"
 #include "ft_printf.h"
+#include "path.h"
 #include <stdlib.h>
 
-int 	go_to_prev_dir()
+static int 	__go_to_prev_dir()
 {
-	return (directory_change_dir(env_get_prev_dir()));
+	return (fs_change_dir(env_get_prev_dir()));
 }
 
-int	go_to_home()
+static int	__got_to_home()
 {
-	return (directory_change_dir(env_get_home()));
+	return (fs_change_dir(env_get_home()));
 }
 
-int	go_to_path(const char *path)
+static t_bool	__update_dir_change_in_env(t_command *cmd)
 {
-	return (directory_change_dir(path));
+	if (!env_set_s("OLDPWD", env_get_s("PWD")))
+		return (FALSE);
+	return (env_set_s("PWD", cmd->argv->argv[1]));
 }
 
-int	builtin_cd(t_command *cmd)
+static int	__got_to_path(t_command *cmd)
+{
+	int ret;
+
+	if (ft_strcmp(cmd->argv->argv[1], "-") || path_contains("CDPATH", cmd->argv->argv[1]))
+	{
+		ret = __go_to_prev_dir();
+		ft_dprintf(cmd->fds[FD_OUT], "%s\n", env_get_current_dir());
+	}
+	else
+		ret = fs_change_dir(cmd->argv->argv[1]);
+	if (ret == 0 && __update_dir_change_in_env(cmd))
+		return (ret);
+	return (ret);
+}
+
+int		builtin_cd(t_command *cmd)
 {
 	if (cmd->argv->argc == 1)
-		return(go_to_home());
+		return(__got_to_home());
 	else if (cmd->argv->argc == 2)
-	{
-		if (cmd->argv->argv[1][0] == '.')
-			return (go_to_path(cmd->argv->argv[1]));	/*check_for_real_path*/
-		else if (ft_strcmp(cmd->argv->argv[1], "-"))
-		{
-			int ret = go_to_prev_dir();
-			ft_dprintf(cmd->fds[FD_OUT], "%s\n", env_get_current_dir());
-			return (ret);
-		}
-		else
-			return (go_to_path(cmd->argv->argv[1]));
-	}
+		return (__got_to_path(cmd));
 	else
 	{
 		/*handle errors*/
