799413f5a6143ad75c0117586c215b97a473ade1
Harou <haroutioun24@gmail.com>
2021-03-13 10:57:16 +0100

    fixed __set bug

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,20 +6,19 @@
 
 static	t_bool	__set(const char *to_be_splited)
 {
-
 	t_bool	ret;
 	t_pair	*to_set;
 
 	ret = TRUE;
 	/*need split_line_into_key_value_pair()->env_singleton ??? */
 	to_set = split_line_into_key_value_pair(to_be_splited);
-	if (!to_set || env_set_s(to_set->f.key, to_set->s.value))
+	if (!to_set || !env_set_s(to_set->f.key, to_set->s.value))
 		ret = FALSE;
 	pair_destroy(to_set);
 	return (ret);
 }
 
-int		builtin_set(t_command *cmd)
+int		builtin_export(t_command *cmd)
 {	
 	int	exit_status;
 	int	count;
