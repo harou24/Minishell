11a17c254a3f365d9605cd0e5ce6072211eccc69
Harou <haroutioun24@gmail.com>
2021-03-14 11:17:40 +0100

    exit_code->exit_status in builtin_exit

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -3,15 +3,15 @@
 
 int	builtin_exit(t_command *cmd)
 {
-	unsigned char	exit_code;
+	unsigned char	exit_status;
 
-	exit_code = EXIT_SUCCESS;
+	exit_status = EXIT_SUCCESS;
 	if (cmd->argv->argc > 2)
 	{
 		/*error handler*/
 		return (-1);
 	}
 	if (cmd->argv->argc == 2)
-		exit_code = (unsigned char)ft_atoi(cmd->argv->argv[1]);
-	exit(exit_code);
+		exit_status = (unsigned char)ft_atoi(cmd->argv->argv[1]);
+	exit(exit_status);
 }
