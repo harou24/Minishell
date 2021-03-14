2576c3667b5a5d396037f8db84f385ec891c86a6
Harou <haroutioun24@gmail.com>
2021-03-14 12:58:09 +0100

    cleaned up

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -5,53 +5,54 @@
 #include "path.h"
 #include <stdlib.h>
 
+#define DIR_HAS_CHANGED 0
+
 static int 	__go_to_prev_dir()
 {
 	return (fs_change_dir(env_get_prev_dir()));
 }
 
-static int	__got_to_home()
+static int	__go_to_home()
 {
 	return (fs_change_dir(env_get_home()));
 }
 
-static t_bool	__update_dir_change_in_env(t_command *cmd)
+static t_bool	__update_dir_change(t_command *cmd)
 {
-	if (!env_set_s("OLDPWD", env_get_s("PWD")))
-		return (FALSE);
-	return (env_set_s("PWD", cmd->argv->argv[1]));
+	return (env_set_s("OLDPWD", env_get_s("PWD")) && 
+			env_set_s("PWD", cmd->argv->argv[1]));
 }
 
-static int	__got_to_path(t_command *cmd)
+static int	__go_to_path(t_command *cmd)
 {
-	int ret;
+	int dir_change;
 
 	if (ft_strcmp(cmd->argv->argv[1], "-") || path_contains("CDPATH", cmd->argv->argv[1]))
 	{
-		ret = __go_to_prev_dir();
+		dir_change = __go_to_prev_dir();
 		ft_dprintf(cmd->fds[FD_OUT], "%s\n", env_get_current_dir());
 	}
 	else
-		ret = fs_change_dir(cmd->argv->argv[1]);
-	if (ret == 0 && __update_dir_change_in_env(cmd))
-		return (ret);
-	return (ret);
+		dir_change = fs_change_dir(cmd->argv->argv[1]);
+	if (dir_change == DIR_HAS_CHANGED)
+		__update_dir_change(cmd);
+	return (dir_change);
 }
 
 int		builtin_cd(t_command *cmd)
 {
-	int	ret;
+	int	dir_change;
 
 	if (cmd->argv->argc == 1)
-		ret = __got_to_home();
+		dir_change = __go_to_home();
 	else if (cmd->argv->argc == 2)
-		ret = __got_to_path(cmd);
+		dir_change = __go_to_path(cmd);
 	else
 	{
 		/*handle errors*/
 		return (-1);
 	}
-	if (ret != 0)
+	if (dir_change != DIR_HAS_CHANGED)
 		return (-1);
 	exit(EXIT_SUCCESS);
 }
