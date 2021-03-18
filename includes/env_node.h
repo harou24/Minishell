45d1ccd823a4f6a24a44690d431f3c33964200f7
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-19 00:11:16 +0100

    tests/src/env_singleton.c: basic test for environment, tests/src/env.cpp: minor fixes

--- includes/env_node.h
+++ includes/env_node.h
@@ -6,27 +6,23 @@
 
 typedef enum	s_scope
 {
-		/*
-			env,export,set without arguments print out for each one of them different "scope".
-			DECLARE,
-			ENV,
-			EXPORT,
-			SET
-		*/
-		LOCAL,
-		ENVIRON
-}		e_scope;
+				SCOPE_LOCAL,
+				SCOPE_ENVIRON
+}				e_scope;
 
 typedef struct	s_env_node
 {
-	char	*value;
-	char	*line;
-	size_t	environ_index; // NOT USED
-	e_scope	scope;
-}		t_env_node;
+	char		*value;
+	char		*line;
+	size_t		environ_index;
+	e_scope		scope;
+}				t_env_node;
 
-t_env_node	*env_node_create(const char *line, e_scope scope);
-void		env_node_destroy(void *to_destroy);
+t_env_node	*env_node_create(char *line, e_scope scope);
+t_env_node	*env_node_destroy(t_env_node *node);
+void		env_node_destroy_hm(void *node);
+
+void		env_node_set_environ_index(t_env_node *node, size_t index);
 
 t_bool		env_node_update(t_env_node *node, char *key, char *value);
 char		*env_node_get_value(t_env_node *node);
--- src/env_node.c
+++ src/env_node.c
@@ -1,37 +1,63 @@
-#include "env_node.h"
-#include "libft.h"
+#include <assert.h>
 #include <stdlib.h>
 
-t_env_node	*env_node_create(const char *line, e_scope scope)
+#include "libft.h"
+
+#include "env_node.h"
+
+t_env_node	*env_node_create(char *line, e_scope scope)
 {
 	t_env_node	*node;
 
+	assert(line);
 	node = ft_calloc(sizeof(t_env_node), 1);
-	if (!node)
-		return (NULL);
-	node->value = ft_strdup(ft_strchr(line, '='));
-	node->line = ft_strdup(line);
-	if (!node->value || !node->line)
-		return (NULL);
-	node->scope = scope;
+	if (node)
+	{
+		node->value = ft_strdup(ft_strchr(line, '='));
+		node->line = ft_strdup(line);
+		node->scope = scope;
+	}
 	return (node);
 }
 
-void		env_node_destroy(void *to_destroy)
+t_env_node	*env_node_destroy(t_env_node *node)
 {
-	t_env_node *node = (t_env_node *)to_destroy;
-	free(node->value);
-	free(node->line);
-	free(node);
+	if (node)
+	{
+		free(node->value);
+		free(node->line);
+		free(node);
+	}
+	return (NULL);
+}
+
+void		env_node_destroy_hm(void *_node)
+{
+	t_env_node *node;
+
+	if (_node)
+	{
+		node = (t_env_node *)_node;
+		free(node->value);
+		free(node->line);
+		free(node);
+	}
+}
+
+void		env_node_set_environ_index(t_env_node *node, size_t index)
+{
+	node->environ_index = index;
 }
 
 char        *env_node_get_value(t_env_node *node)
 {
+	assert(node);
 	return (node->value);
 }
 
 e_scope     env_node_get_scope(t_env_node *node)
 {
+	assert(node);
 	return (node->scope);
 }
 
