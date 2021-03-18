7f819489ead4396c54715f62a303c5256d385325
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-18 22:21:59 +0100

    environ.c: minor corrections

--- src/environ.c
+++ src/environ.c
@@ -18,7 +18,7 @@ t_bool		environ_init(char **environ)
 
 t_bool		environ_deinit()
 {
-	return (!g_environ_vec__ || vector(&g_environ_vec__, V_DESTROY, 0, NULL) == NULL);
+	return (!g_environ_vec__ || vector(&g_environ_vec__, V_DESTROY, TRUE, NULL) == NULL);
 }
 
 
@@ -38,6 +38,7 @@ t_bool		environ_set(size_t index, char *line)
 t_bool		environ_add(char *line)
 {
 	assert(g_environ_vec__);
+	assert(line);
 	vector(&g_environ_vec__, V_POPBACK, 0, NULL);
 	return (vector(&g_environ_vec__, V_PUSHBACK, 0, line) != NULL
 			&& vector(&g_environ_vec__, V_PUSHBACK, 0, NULL) != NULL);
