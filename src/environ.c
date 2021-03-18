7d66458fb16550a41cd3cc47b1c26f1c1ecb3279
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-18 22:18:22 +0100

    environ.c/h: created for environ access and mutation

---
+++ src/environ.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <assert.h>
+
+#include "debugger.h"
+#include "libft.h"
+#include "vector.h"
+
+#include "environ.h"
+
+static void	*g_environ_vec__;
+
+t_bool		environ_init(char **environ)
+{
+	return (g_environ_vec__
+		|| (vector(&g_environ_vec__, V_ADOPT, 0, environ)
+		&& vector(&g_environ_vec__, V_PUSHBACK, 0, NULL)));
+}
+
+t_bool		environ_deinit()
+{
+	return (!g_environ_vec__ || vector(&g_environ_vec__, V_DESTROY, 0, NULL) == NULL);
+}
+
+
+char		**environ_get()
+{
+	return (g_environ_vec__ ? (char **)vector(&g_environ_vec__, V_MEM, 0, NULL) : NULL);
+}
+
+t_bool		environ_set(size_t index, char *line)
+{
+	assert(g_environ_vec__);
+	assert(line);
+	free(vector(&g_environ_vec__, V_POPAT, index, NULL));
+	return (vector(&g_environ_vec__, V_PUSHAT, index, line) != NULL);
+}
+
+t_bool		environ_add(char *line)
+{
+	assert(g_environ_vec__);
+	vector(&g_environ_vec__, V_POPBACK, 0, NULL);
+	return (vector(&g_environ_vec__, V_PUSHBACK, 0, line) != NULL
+			&& vector(&g_environ_vec__, V_PUSHBACK, 0, NULL) != NULL);
+}
+
+void		environ_remove(size_t index)
+{
+	assert(g_environ_vec__);
+	vector(&g_environ_vec__, V_POPAT, index, NULL);
+}
