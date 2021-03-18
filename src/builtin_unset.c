63fe04fe5a7b015f8bfef9aba8375991c9725e8a
Harou <haroutioun24@gmail.com>
2021-03-18 23:26:21 +0100

    count->index

--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -4,7 +4,7 @@
 
 int	builtin_unset(t_command *cmd)
 {
-	int	count;
+	int	index;
 	int	exit_status;
 
 	if (cmd->argv->argc < 2)
@@ -13,13 +13,13 @@ int	builtin_unset(t_command *cmd)
 		ft_dprintf(STDERR, "unset: not enough arguments");
 		return (-1);
 	}
-	count = 1;
+	index = 1;
 	exit_status = EXIT_SUCCESS;
-	while (count < cmd->argv->argc)
+	while (index < cmd->argv->argc)
 	{
-		if (!env_unset_s(cmd->argv->argv[count]))
+		if (!env_unset_s(cmd->argv->argv[index]))
 			exit_status = EXIT_FAILURE;
-		count++;
+		index++;
 	}
 	exit(exit_status);
 }
