8a9215fea24a3904cce51b8cdfdcaf8b6cdaf932
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-18 14:21:24 +0100

    command.c/h: removed filedescriptor handling from t_command -> handled by t_execscheme

--- src/command.c
+++ src/command.c
@@ -15,8 +15,6 @@ t_command	*command_create(char *path, t_argv *argv)
 	{
 		cmd->path = path;
 		cmd->argv = argv;
-		cmd->fds[FD_IN] = STDIN; /* default */
-		cmd->fds[FD_OUT] = STDOUT; /* default */
 	}
 	return (cmd);
 }
@@ -32,13 +30,6 @@ t_command	*command_destroy(t_command **cmd)
 		free(*cmd);
 	}
 	return ((*cmd = NULL));
-}
-
-void		command_set_fds(t_command *cmd, int fd_in, int fd_out)
-{
-	assert(cmd);
-	cmd->fds[FD_IN] = fd_in;
-	cmd->fds[FD_OUT] = fd_out;
 }
 
 void		command_pretty_dump(t_command *cmd, int indent)
