a63142ad31268e0a8bf8e31ff8aa1641dfcf921f
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-25 14:47:22 +0100

    executor.c: added conditional logic for redirection to execution

--- src/executor.c
+++ src/executor.c
@@ -57,15 +57,16 @@ int			executor_launch_processes(t_execscheme *scheme)
 			error = executor_launch_parallel_scheme(scheme, p_tab_at(pid_index));
 			if (error != 0)
 				break;
+			pid_index++;
 		}
-		else
+		else if (!(scheme->rel_type[PREV_R] & (REL_READ | REL_APPEND | REL_WRITE)))
 		{
 			/* sequential operation */
 			error = executor_launch_sequential_scheme(scheme, p_tab_at(pid_index));
 			if (error != 0)
 				break;
+			pid_index++;
 		}
-		pid_index++;
 		scheme = scheme->next;
 	}
 	return (error);
@@ -76,12 +77,20 @@ t_bool		executor_prepare_processes(t_execscheme *scheme)
 	while (scheme)
 	{
 		/* dispatch for execscheme type */
-		dbg("executing scheme: %s, %s <- relation -> %s\n", execscheme_dump_op_type(scheme->op_type), execscheme_dump_relation_type(scheme->rel_type[PREV_R]), execscheme_dump_relation_type(scheme->rel_type[NEXT_R]));
-		if (execscheme_dispatch(scheme->rel_type[NEXT_R])(scheme) != 0)
+
+		if (!(scheme->rel_type[PREV_R] & (REL_READ | REL_APPEND | REL_WRITE)))
 		{
-			dbg("%s\n", "failed to execute scheme !");
-			p_tab_signal_all(SIGTERM);
-			return (FALSE);
+			dbg("executing scheme: %s, %s <- relation -> %s\n", execscheme_dump_op_type(scheme->op_type), execscheme_dump_relation_type(scheme->rel_type[PREV_R]), execscheme_dump_relation_type(scheme->rel_type[NEXT_R]));
+			if (execscheme_dispatch(scheme->rel_type[NEXT_R])(scheme) != 0)
+			{
+				dbg("%s\n", "failed to execute scheme !");
+				p_tab_signal_all(SIGTERM);
+				return (FALSE);
+			}
+		}
+		else
+		{
+			dbg("skipped scheme: %s, %s <- relation -> %s\n", execscheme_dump_op_type(scheme->op_type), execscheme_dump_relation_type(scheme->rel_type[PREV_R]), execscheme_dump_relation_type(scheme->rel_type[NEXT_R]));
 		}
 		scheme = scheme->next;
 	}
