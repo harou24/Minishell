41f1711f40edfc9666bc84cddd7a48e593f58505
Harou <haroutioun24@gmail.com>
2021-03-25 19:02:38 +0100

    cd update

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -1,11 +1,16 @@
 #include "bash_ops.h"
 #include "env_access.h"
 #include "filesystem_traversal.h"
+#include "filesystem.h"
 #include "ft_printf.h"
 #include "path.h"
 #include <stdlib.h>
+#include <limits.h>
 
-#define DIR_ERROR 0
+#include <errno.h>
+#include <string.h>
+
+#define NO_ERROR_OCCURED 0
 #define TOO_MANY_ARGS -1
 
 static int 	__go_to_prev_dir()
@@ -25,8 +30,8 @@ static int	__go_to_path(const char *path)
 
 static t_bool	__update_dir_change(char *new_path)
 {
-	return (env_set_s("OLDPWD", env_get_s("PWD")) && 
-			env_set_s("PWD", new_path));
+	return (env_set_s("OLDPWD", env_get_s("PWD"), SCOPE_ENVIRON) && 
+			env_set_s("PWD", new_path, SCOPE_ENVIRON));
 }
 
 static	t_bool	__is_dash(const char *str)
@@ -38,7 +43,6 @@ static t_bool __is_path_in_cdpath(const char *path)
 {
 	return (path_contains("CDPATH", path));
 }
-
 static t_bool	__is_print_path_needed(const char *path)
 {
 	return (__is_dash(path) || __is_path_in_cdpath(path));
@@ -49,38 +53,90 @@ static	void	__print_cur_path()
 	ft_dprintf(STDOUT, "%s\n", env_get_current_dir());
 }
 
-static int	__exec_cd(t_command *cmd)
+char	*ft_realpath(char *path)
 {
-	int error;
-
-	if (cmd->argv->argc == 1)
-		error = __go_to_home();
-	else if (cmd->argv->argc == 2)
-	{
-		if (__is_dash(cmd->argv->argv[1]))
-			error = __go_to_prev_dir();
-		else
-			error = __go_to_path(cmd->argv->argv[1]);
-	}
-	else
-		error = TOO_MANY_ARGS;
-	return (error);
+	char resolved_path[1024];
+	realpath(path, resolved_path);
+	/*ft_printf("REALPATH: errno: %d | errno_str: %s | realpath ret:%s\n", errno, strerror(errno), test);*/
+	return (ft_strdup(resolved_path));
 }
 
-int		builtin_cd(t_command *cmd)
+static int	__exec_cd_home()
 {
 	int	error;
+	int	exit_status;
+	char	*new_path;
 
-	if (cmd->argv->argc > 0 && cmd->argv->argc <= 2)
-		error = __exec_cd(cmd);
-	else
-	{
-		/*handle errors*/
-		exit(EXIT_FAILURE);
+	error = __go_to_home();
+	exit_status = EXIT_FAILURE;
+	new_path = NULL;
+	if (error == NO_ERROR_OCCURED)
+		new_path = env_get_home();
+	if (new_path && __update_dir_change(new_path))
+		exit_status = EXIT_SUCCESS;
+	return (exit_status);
+}
+
+static int	__exec_cd_dash()
+{
+	int	error;
+	int	exit_status;
+	char	*new_path;
+
+	error = __go_to_prev_dir();
+	exit_status = EXIT_FAILURE;
+	new_path = NULL;
+	if (error == NO_ERROR_OCCURED)
+		new_path = env_get_prev_dir();
+	if (new_path && __update_dir_change(new_path))
+		exit_status = EXIT_SUCCESS;
+	return (exit_status);
+}
+
+static int	__exec_cd(t_command *cmd)
+{
+	int	error;
+	int	exit_status;
+	char	*new_path;
+
+	exit_status = EXIT_FAILURE;
+	if (__is_path_in_cdpath(cmd->argv->argv[1]))
+	{		/*NOT WORKING*/
+		new_path = path_expand("CDPATH", cmd->argv->argv[1]);
+		ft_printf("PATH->%s\n",new_path);
 	}
-	if (!(error == DIR_ERROR && __update_dir_change(cmd->argv->argv[1])))
-		exit(EXIT_FAILURE);
-	if (__is_print_path_needed(cmd->argv->argv[1]))
-		__print_cur_path();
-	exit(EXIT_SUCCESS);
+	else
+		new_path = ft_realpath(cmd->argv->argv[1]);
+	error = __go_to_path(new_path);
+	if (new_path && error == NO_ERROR_OCCURED && __update_dir_change(new_path))
+		exit_status = EXIT_SUCCESS;
+	else if (!fs_has_type(new_path, F_DIR))
+		ft_dprintf(STDOUT, "cd: not a directory: %s\n", cmd->argv->argv[1]);
+	else
+		ft_dprintf(STDOUT, "cd: no such file or directory: %s\n", cmd->argv->argv[1]);
+	free(new_path);
+	return (exit_status);
+}
+
+int	builtin_cd(t_command *cmd)
+{
+	int exit_status;
+
+	exit_status = EXIT_FAILURE;
+	if (cmd->argv->argc > 0 && cmd->argv->argc <= 2)
+	{
+		if (cmd->argv->argc == 1)
+			exit_status = __exec_cd_home();
+		else
+		{
+			if (__is_dash(cmd->argv->argv[1]))
+				exit_status = __exec_cd_dash();
+			else
+				exit_status = __exec_cd(cmd);	
+			if (__is_print_path_needed(cmd->argv->argv[1]))
+				__print_cur_path();
+		}
+	}
+	/*__print_cur_path();*/
+	exit(exit_status);
 }
