5ae1d784a2ac73ecef25a7503df171cdb9d14d16
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-03-26 21:16:38 +0100

    process.c: changed p_register_sighandler() struct sigaction sa_flags now is SA_RESTART to properly restart functions on CTRL-c

--- src/process.c
+++ src/process.c
@@ -24,7 +24,7 @@ int		p_register_signalhandler(int sig, void (*sighandler)(int sig))
 
 	sa.sa_handler = sighandler;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0; /* SA_RESTART would restart function that was interrupted */
+	sa.sa_flags = SA_RESTART;
 	if (sigaction(sig, &sa, NULL) == -1)
 	{
 		dbg("couldn't register handler for signal %s, errno: %s\n", strsignal(sig), strerror(errno));
