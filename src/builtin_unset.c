d820ee9730710d62d23715aabf42baeb806bb9ef
Haroutioun <61208023+harou24@users.noreply.github.com>
2021-03-29 17:55:43 +0200

    unset (#144)
    * unset
    
    * env.cpp test: REQUIRE -> CHECK
    
    * dispatch: added builtin unset
    
    * environ.c: freeing when removing
    
    * env.c: unset also removes from environ
    
    * builtin_unset.c: return instead of exit
    
    Co-authored-by: s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>

---
+++ src/builtin_unset.c
@@ -0,0 +1,25 @@
+#include "bash_ops.h"
+#include "env_singleton.h"
+#include "ft_printf.h"
+
+int	builtin_unset(t_command *cmd)
+{
+	int	index;
+	int	exit_status;
+
+	if (cmd->argv->argc < 2)
+	{
+		/*later use error handler ?*/
+		ft_dprintf(STDERR, "unset: not enough arguments");
+		return (-1);
+	}
+	index = 1;
+	exit_status = EXIT_SUCCESS;
+	while (index < cmd->argv->argc)
+	{
+		if (!env_unset_s(cmd->argv->argv[index]))
+			exit_status = EXIT_FAILURE;
+		index++;
+	}
+	return(exit_status);
+}
