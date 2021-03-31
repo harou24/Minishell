502dfbdd704dd6202b155291c2059b9d1c0f50ee
Harou <haroutioun24@gmail.com>
2021-03-31 12:20:53 +0200

    norminette -> command.c debugger.c env.c env_access.c environ.c

--- src/env_access.c
+++ src/env_access.c
@@ -1,39 +1,37 @@
 #include "ft_unistd.h"
 #include "env_access.h"
 
-char	*env_get_user()
+char	*env_get_user(void)
 {
 	return (env_get_s("USER"));
 }
 
-char	*env_get_host()
+char	*env_get_host(void)
 {
 	return (ft_gethostname());
-	/* POSIX does not require HOST to be set
-	return (env_get_s("HOST")); */
 }
 
-char	*env_get_current_dir()
+char	*env_get_current_dir(void)
 {
 	return (env_get_s("PWD"));
 }
 
-char	*env_get_prev_dir()
+char	*env_get_prev_dir(void)
 {
 	return (env_get_s("OLDPWD"));
 }
 
-char	*env_get_home()
+char	*env_get_home(void)
 {
 	return (env_get_s("HOME"));
 }
 
-char	*env_get_path()
+char	*env_get_path(void)
 {
 	return (env_get_s("PATH"));
 }
 
-char	*env_get_cdpath()
+char	*env_get_cdpath(void)
 {
 	return (env_get_s("CDPATH"));
 }
