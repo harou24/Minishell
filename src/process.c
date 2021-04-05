03992d4d1bcd2d18e47ce8c40c7a43bdc6ccd9b2
Harou <haroutioun24@gmail.com>
2021-04-05 21:40:57 +0200

    process -> ok

--- src/process.c
+++ src/process.c
@@ -12,27 +12,6 @@
 #include "vector.h"
 #include "process.h"
 
-pid_t	*p_allocate_pid(pid_t pid)
-{
-	return ((pid_t *)ft_memdup(&pid, sizeof(pid_t)));
-}
-
-int	p_register_signalhandler(int sig, void (*sighandler)(int sig))
-{
-	struct sigaction	sa;
-
-	sa.sa_handler = sighandler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(sig, &sa, NULL) == -1)
-	{
-		dbg("couldn't register handler for signal %s, errno: %s\n",
-			strsignal(sig), strerror(errno));
-		return (-1);
-	}
-	return (0);
-}
-
 int	p_pause(void)
 {
 	pause();
