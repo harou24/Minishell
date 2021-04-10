cb2a408abce61afdf2f3e028399d1a6f52c90b0e
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-04-10 12:33:32 +0200

    process_queue.c: register for signal unconditionally

--- src/process_queue.c
+++ src/process_queue.c
@@ -16,25 +16,23 @@ static volatile size_t	g_ready__;
 
 static void	__sighandler(int signal)
 {
+	if (signal == SIGUSR1)
+		g_ready__++;
 	dbg("Pid %i received signal %s\n", getpid(), strsignal(signal));
-	g_ready__++;
 }
 
 void	p_queue_register_signalhandler(int sig)
 {
-	static t_bool	sighandler_registered;
-
-	if (!sighandler_registered)
-	{
-		p_register_signalhandler(sig, __sighandler);
-		sighandler_registered = TRUE;
-	}
 	g_ready__ = 0;
+	p_register_signalhandler(sig, __sighandler);
+	dbg("Registed sighandler for signal %i\n", sig);
 }
 
 void	p_queue_wait_for_signals(size_t signalcount)
 {
+	dbg("Waiting until signalcounter{%i} has reached signalcount{%i}\n", g_ready__, signalcount);
 	while (g_ready__ < signalcount)
 	{
 	}
+	dbg("Signalcounter{%i} has reached signalcount{%i}!\n", g_ready__, signalcount);
 }
