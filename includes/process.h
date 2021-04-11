8a3324e265d3d90b3f2d52522e4f61a2d28e0b26
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-04-11 15:37:42 +0200

    proccess, various -> reworked pid logic into execscheme

--- includes/process.h
+++ includes/process.h
@@ -17,26 +17,24 @@ typedef enum	e_waitcond
 	W_ABORTED
 }				t_waitcond;
 
-/* process_tab.c */
+// process_tab.c
+/*
 t_bool	p_tab_push(pid_t pid);
 pid_t	p_tab_at(size_t index);
 size_t	p_tab_size();
+t_bool	p_tab_drop(pid_t pid);
 
-int		p_tab_waitpid_all(t_waitcond cond);
+int		p_tab_wait_all(t_waitcond cond);
 int		p_tab_signal_all(int signal);
-
-/* process_queue.c */
-void	p_queue_register_signalhandler(int sig);
-void	p_queue_wait_for_signals(size_t signalcount);
+int		p_tab_drop_all();
+*/
 
 /* process.c */
 pid_t	*p_allocate_pid(pid_t pid);
 
 int		p_register_signalhandler(int sig, void (*sighandler)(int sig));
-int		p_pause();
 
-int		p_waitpid(pid_t pid, t_waitcond cond);
+int		p_wait(pid_t *pid, t_waitcond cond);
 int		p_signal(pid_t pid, int sig);
-
 
 #endif
