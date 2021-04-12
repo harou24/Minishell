390cad6bd8d8b8b94469a0a4c2fbdba59e7cf191
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-04-12 03:15:55 +0200

    buildin_cd.c/bash_ops.h: fixed memory leak, heap-buffer overflow, paths with ~/

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -1,93 +1,92 @@
-#include "bash_ops.h"
-#include "env_access.h"
-#include "filesystem_traversal.h"
-#include "filesystem.h"
-#include "ft_printf.h"
-#include "path.h"
+#include <assert.h>
 #include <stdlib.h>
 #include <limits.h>
 
 #include <errno.h>
 #include <string.h>
 
-#define NO_ERROR_OCCURED 0
-#define TOO_MANY_ARGS -1
+#include "libft.h"
 
-static int	__exec_cd_home(void)
+#include "bash_ops.h"
+#include "env_access.h"
+#include "filesystem_traversal.h"
+#include "filesystem.h"
+#include "ft_printf.h"
+#include "path.h"
+
+static int	update_dir_change(char *new_path)
 {
-	int		error;
-	int		exit_status;
-	char	*new_path;
-
-	error = __go_to_home();
-	exit_status = EXIT_FAILURE;
-	new_path = NULL;
-	if (error == NO_ERROR_OCCURED)
-		new_path = ft_strdup(env_get_home());
-	if (new_path && __update_dir_change(new_path))
-		exit_status = EXIT_SUCCESS;
-	return (exit_status);
+	char *new_path_tmp = ft_strdup(new_path);
+	env_set_s("OLDPWD", env_get_current_dir(), SCOPE_ENVIRON);
+	env_set_s("PWD", new_path_tmp, SCOPE_ENVIRON);
+	free(new_path_tmp);
+	return (0);
 }
 
-static int	__exec_cd_dash(void)
+static int	cd(char *fname)
 {
-	int		error;
-	int		exit_status;
-	char	*new_path;
-
-	error = __go_to_prev_dir();
-	exit_status = EXIT_FAILURE;
-	new_path = NULL;
-	if (error == NO_ERROR_OCCURED)
-		new_path = ft_strdup(env_get_prev_dir());
-	if (new_path && __update_dir_change(new_path))
-		exit_status = EXIT_SUCCESS;
-	return (exit_status);
+	if (fs_change_dir(fname) == 0 && update_dir_change(fname) == 0)
+		return (0);
+	return (-1);
 }
 
-static int	__exec_cd(t_command *cmd)
+static int	__exec_cd(char *fname)
 {
-	int		error;
-	int		exit_status;
 	char	*new_path;
 
-	exit_status = EXIT_FAILURE;
-	if (__is_path_in_cdpath(cmd->argv->argv[1]))
-		new_path = path_expand(env_get_s("CDPATH"), cmd->argv->argv[1]);
+	if (path_contains(env_get_cdpath(), fname))
+	{
+		new_path = path_expand(env_get_cdpath(), fname);
+		ft_dprintf(STDOUT, "%s\n", new_path);
+	}
 	else
-		new_path = ft_realpath(cmd->argv->argv[1]);
-	error = __go_to_path(new_path);
-	if (new_path && error == NO_ERROR_OCCURED && __update_dir_change(new_path))
-		exit_status = EXIT_SUCCESS;
-	else if (!fs_has_type(new_path, F_DIR))
-		ft_dprintf(STDOUT, "cd: not a directory: %s\n", cmd->argv->argv[1]);
-	else
-		ft_dprintf(STDOUT, "cd: no such file or directory: %s\n",
-			cmd->argv->argv[1]);
+		new_path = ft_realpath(fname);
+	if (cd(new_path) != 0)
+	{
+		if (!directory_exists(new_path))
+			ft_dprintf(STDOUT, "cd: not a directory: %s\n", fname);
+		else if (!directory_is_openable(new_path))
+			ft_dprintf(STDOUT, "cd: no such file or directory: %s\n",
+				fname);
+		free(new_path);
+		return (-1);
+	}
 	free(new_path);
-	return (exit_status);
+	return (0);
+}
+
+static int __handle_cd_tilde(char *fname)
+{
+	char	*path;
+
+	assert(fname);
+	if (ft_strncmp("~", fname, 2) == 0 || ft_strncmp("~/", fname, 3) == 0)
+		return (cd(env_get_home()));
+	path = ft_strjoin_multi(3, env_get_home(), "/", fname + 2);
+	if (!path)
+		return (-1);
+	if (cd(path) == 0)
+	{
+		free(path);
+		return (0);
+	}
+	free(path);
+	return (-1);
 }
 
 int	builtin_cd(t_command *cmd)
 {
-	int	exit_status;
-
-	exit_status = EXIT_FAILURE;
-	if (cmd->argv->argc > 0 && cmd->argv->argc <= 2)
+	if (cmd->argv->argc == 1 || ft_strncmp("~", cmd->argv->argv[1], 1) == 0)
+		return (__handle_cd_tilde(cmd->argv->argv[1]));
+	else if (cmd->argv->argc == 2)
 	{
-		if (cmd->argv->argc == 1)
-			exit_status = __exec_cd_home();
-		else
+		if (ft_strncmp("-", cmd->argv->argv[1], 2) == 0)
 		{
-			if (__is_tilde(cmd->argv->argv[1]))
-				exit_status = __exec_cd_home();
-			else if (__is_dash(cmd->argv->argv[1]))
-				exit_status = __exec_cd_dash();
-			else
-				exit_status = __exec_cd(cmd);
-			if (__is_print_path_needed(cmd->argv->argv[1]))
-				__print_cur_path();
+			ft_dprintf(STDOUT, "%s\n", env_get_prev_dir());
+			return (cd(env_get_prev_dir()));
 		}
+		else
+			return (__exec_cd(cmd->argv->argv[1]));
 	}
-	return (exit_status);
+	return (-1);
 }
