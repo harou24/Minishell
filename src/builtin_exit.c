7d4bbca5686eff9a70e94f0edf4ddce3deaaaa74
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-04-14 12:04:00 +0200

    builtin_exit.c: fixed for bash alike usage

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -1,16 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   builtin_exit.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sverschu <sverschu@student.codam.n>          +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/04/13 20:57:32 by sverschu      #+#    #+#                 */
+/*   Updated: 2021/04/13 20:57:34 by sverschu      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "bash_ops.h"
 #include <stdlib.h>
 
 int	builtin_exit(t_command *cmd)
 {
-	unsigned char	exit_status;
+	unsigned int	exit_status;
 
-	exit_status = EXIT_SUCCESS;
-	if (cmd->argv->argc > 2)
-	{
-		exit(EXIT_FAILURE);
-	}
 	if (cmd->argv->argc == 2)
-		exit_status = (unsigned char)ft_atoi(cmd->argv->argv[1]);
-	exit(exit_status);
+	{
+		exit_status = ft_atoi(cmd->argv->argv[1]);
+		if (exit_status > 255)
+			exit (255);
+		exit (exit_status);
+	}
+	else if (cmd->argv->argc == 1)
+		exit(0);
+	exit(128);
 }
