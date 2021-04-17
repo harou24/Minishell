0c3fac84ce25005ca58e8e18dce03d9dd75ea7cd
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-04-17 14:30:51 +0200

    debugger.h: condensed output of dbg() macro

--- includes/debugger.h
+++ includes/debugger.h
@@ -33,14 +33,14 @@ t_bool				debug_init_tofd(int fd);
 void				debug_deinit(void);
 int					debug(const char *format, ...);
 
-# define DBG_FILENAME_LEN 55
+# define DBG_FILENAME_LEN 60
 # define DBG_LINE_LEN 5
-# define DBG_FUNCNAME_LEN 35
+# define DBG_FUNCNAME_LEN 30
 
 # define dbg(fmt, ...) \
-        do { if (DEBUG) debug("%-*s%*d%*s(): " fmt, \
-				DBG_FILENAME_LEN, __FILE__, \
+        do { if (DEBUG) debug("%-*.*s%*d%*.*s(): " fmt, \
+				DBG_FILENAME_LEN, DBG_FILENAME_LEN, __FILE__, \
 				DBG_LINE_LEN, __LINE__, \
-				DBG_FUNCNAME_LEN, __func__, \
+				DBG_FUNCNAME_LEN, DBG_FUNCNAME_LEN, __func__, \
 				__VA_ARGS__); } while (0)
 #endif
