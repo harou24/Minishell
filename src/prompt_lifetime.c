0d822eced2f189df9e2861fb25ff9bfadaad7a55
Haroutioun <haroutioun24@gmail.com>
2021-05-18 14:54:47 +0200

    prompt_lifetime.c normed

--- src/prompt_lifetime.c
+++ src/prompt_lifetime.c
@@ -15,7 +15,7 @@ void	prompt_destroy(t_prompt *_prompt)
 	free(_prompt->current_path);
 	free(_prompt->buffer);
 	history_destroy(_prompt->hist);
-    cursor_destroy(_prompt->cursor);
+	cursor_destroy(_prompt->cursor);
 	free(_prompt);
 }
 
@@ -36,9 +36,10 @@ t_prompt 	*prompt_create(const char *username, const char *hostname)
 	prompt->buffer = ft_calloc(sizeof(char), __PROMPT_BUFF_SIZE);
 	prompt->current_path = fs_get_cur_dir_name();
 	prompt->hist = history_create();
-    prompt->cursor = cursor_create();
+	prompt->cursor = cursor_create();
 	if (!prompt->current_path || !prompt->buffer
-		|| !prompt->username || !prompt->hostname || !prompt->hist || !prompt->cursor)
+		|| !prompt->username || !prompt->hostname
+		|| !prompt->hist || !prompt->cursor)
 	{
 		prompt_destroy(prompt);
 		return (NULL);
